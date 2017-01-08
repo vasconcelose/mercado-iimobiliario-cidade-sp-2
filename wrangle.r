af3612e9c0ae8cf09ec3c49458ed7d252cee0877
vasconcelose <vasconcelos.esm@gmail.com>
2017-01-08 09:55:42 -0200

    depois de calcular rhos

--- wrangle.r
+++ wrangle.r
@@ -4,33 +4,44 @@ library(ggplot2)
 
 df <- read.csv('dados-fixed.csv')
 
-# gg1
-gg1 <- ggplot(aes(x=a, y=fam_endividadas, color=vso), data=df) +
-		geom_jitter(size=5) + 
-		stat_smooth(span=0.95, method='loess', color='black') +
-		scale_color_gradient(low='maroon', high='green') +
-		theme_bw() +
-		xlab('ano') + ylab('taxa de familias endividadas')
-ggsave('graficos/gg1.png')
+# # gg1
+# gg1 <- ggplot(aes(x=a, y=fam_endividadas, color=vso), data=df) +
+# 		geom_jitter(size=5) + 
+# 		stat_smooth(span=0.95, method='loess', color='black') +
+# 		scale_color_gradient(low='maroon', high='green') +
+# 		theme_bw() +
+# 		xlab('ano') + ylab('taxa de familias endividadas')
+# ggsave('graficos/gg1.png')
 
-# gg2
-gg2 <- ggplot(aes(x=vso, y=porc_divida_financ_casa), data=df) +
-		geom_jitter(size=5, shape='O', color='#5378F3') +
-		stat_smooth(method='loess', color='black', span=0.95) +
-		theme_bw() + xlab('vso') + ylab('divida devido ao financiamento de moradia') +
-ggsave('graficos/gg2.png')
+# # gg2
+# gg2 <- ggplot(aes(x=vso, y=porc_divida_financ_casa), data=df) +
+# 		geom_jitter(size=5, shape='O', color='#5378F3') +
+# 		stat_smooth(method='loess', color='black', span=0.95) +
+# 		theme_bw() + xlab('vso') + ylab('divida devido ao financiamento de moradia') +
+# ggsave('graficos/gg2.png')
 
-# gg3
-gg3 <- ggplot(aes(x=a, y=porc_divida_financ_casa), data=df) +
-		geom_jitter(size=6, shape='+', color='#F37853') +
-		stat_smooth(method='loess', color='black', span=0.95) +
-		theme_bw() + xlab('ano') + ylab('divida devido ao financiamento de moradia') +
-ggsave('graficos/gg3.png')
+# # gg3
+# gg3 <- ggplot(aes(x=a, y=porc_divida_financ_casa), data=df) +
+# 		geom_jitter(size=6, shape='+', color='#F37853') +
+# 		stat_smooth(method='loess', color='black', span=0.95) +
+# 		theme_bw() + xlab('ano') + ylab('divida devido ao financiamento de moradia') +
+# ggsave('graficos/gg3.png')
 
-# gg4
-dfTmp <- df
-dfTmp['mi.hab'] = signif(dfTmp$pop / 1e6, digits=3)
-gg4 <- ggplot(aes(x=a, y=pib, size=mi.hab), data=dfTmp) +
-		geom_line(color='#954763') + theme_bw() +
-		xlab('ano') + ylab('pib municipal')
-ggsave('graficos/gg4.png')
+# # gg4
+# dfTmp <- df
+# dfTmp['mi.hab'] = signif(dfTmp$pop / 1e6, digits=3)
+# dfTmp$pib = signif(dfTmp$pib * 100, digits=3)
+# gg4 <- ggplot(aes(x=a, y=pib, size=mi.hab), data=dfTmp) +
+# 		geom_line(color='#954763') + theme_bw() +
+# 		xlab('ano') + ylab('pib municipal (bi)')
+# ggsave('graficos/gg4.png')
+
+# estatisticas
+# correlacao de pearson: vso x divida
+cor.test(df$vso, df$fam_endividadas, method='pearson')
+
+# correlacao de pearson: vso x divida especifica moradia
+cor.test(df$vso, df$porc_divida_financ_casa, method='pearson')
+
+# correlacao de pearson: divida x divida especifica moradia
+cor.test(df$fam_endividadas, df$porc_divida_financ_casa, method='pearson')
